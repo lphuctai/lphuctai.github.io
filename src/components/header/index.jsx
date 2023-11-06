84cec5601a3024917af1cdbec0eccf64181db4bc
lphuctai <lphuctai@gmail.com>
2023-11-06 23:34:33 +0700

    Complete Footer

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,15 +1,14 @@
+import Link from 'next/link';
 import data from '@/data/header';
-import Link from 'next/link'
 
-export default function Header () {
+const Header = () => {
   return (
     <header>
       <nav className="bg-white text-gray-900 py-2 md:py-4 font-sans">
-        <div className="container px-4 mx-auto md:flex md:items-center">
-
+        <div className="max-w-screen-xl container px-4 mx-auto md:flex md:items-center">
           <div className="flex justify-between items-center">
             <h1 className="text-5xl tracking-tighter">
-              <span className="bg-clip-text text-transparent bg-gradient-to-r from-sky-400 to-blue-600 text-5xl font-bold">
+              <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-600 to-blue-600 text-5xl font-medium">
                 {data.blogName}
               </span>
             </h1>
@@ -17,7 +16,7 @@ export default function Header () {
 
           <div className="hidden md:flex flex-col md:flex-row md:ml-auto mt-3 md:mt-0 tracking-tight" id="navbar-collapse">
             {data.routing.map(({ name, uri }, i) => (
-              <Link key={i} href={uri} className="p-2 lg:px-4 font-medium md:mx-2 hover:text-transparent text-2xl transition duration-150 ease-in hover:ease-out bg-clip-text bg-gradient-to-r from-purple-600 to-blue-500">
+              <Link key={i} href={uri} className="p-2 lg:px-4 md:mx-2 text-gray-700 hover:scale-105 hover:transform-gpu hover:text-transparent text-xl transition duration-150 ease-in hover:ease-out bg-clip-text bg-gradient-to-r from-purple-600 to-blue-500">
                 {name}
               </Link>
             ))}
@@ -27,3 +26,5 @@ export default function Header () {
     </header>
   );
 }
+
+export default Header;
