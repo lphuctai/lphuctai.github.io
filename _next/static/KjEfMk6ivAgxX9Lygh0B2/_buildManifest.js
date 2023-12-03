45384b4269942ede5b85a2e526dcce21e54dc6aa
lphuctai <lphuctai@gmail.com>
2023-12-03 23:56:22 +0700

    Update subtree

---
+++ _next/static/KjEfMk6ivAgxX9Lygh0B2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-453a955e6979f799.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
